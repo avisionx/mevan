89a91f0fc4432c951cb0535915bce85837b9d9d8
avisionx <avi17223@iiitd.ac.in>
2019-06-02 14:59:01 +0530

    askfugk

--- js/prof.js
+++ js/prof.js
@@ -50,7 +50,7 @@ function openModal(profId) {
       $("#profComments").html($("#profComments").html() + s);
     }
 
-    $('#commentForm').html('<label class="sr-only" for="inlineFormInputName2">Comment</label><input type="text" class="form-control mb-2 mr-sm-2" style="width: 85%" id="submitThisComment" placeholder="Comment"><button type="submit" class="btn btn-primary mb-2 ml-auto shadow-none" onclick="submitComment(\'' + profId + '\')">Submit</button>')
+    $('#commentForm').html('<label class="sr-only" for="inlineFormInputName2">Comment</label><input type="text" class="form-control mb-2 mr-sm-2" style="width: 85%" id="submitThisComment" placeholder="Comment"><button type="submit" class="btn btn-primary mb-2 ml-auto shadow-none" onclick="event.preventDefault();submitComment(\'' + profId + '\')">Submit</button>')
 
   });
 
